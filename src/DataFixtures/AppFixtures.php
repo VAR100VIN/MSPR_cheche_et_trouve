db78f8e0fe586c8da120803e1d0a18a90798b37c
Bastienlsg <bastien.lassaigne@gmail.com>
2022-12-15 19:17:34 +0100

    ajout du css

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,44 +2,85 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Description;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Plant;
 use App\Entity\User;
 use App\Entity\PlantImages;
+use App\Entity\Image;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager){
+    public function load(ObjectManager $manager){   
         // $product = new Product();
         // $manager->persist($product);
-
+        
 
             $data = './src/DataFixtures/Ressources/Models/plants.json';
             $json = file_get_contents($data);
-            print_r($json);
+           
             $json2 = json_decode($json,true);
 
-            print_r($json2);
-
-
-
+         
+   
+            foreach ($json2 as $plant){
+                $tableauplant[]=$plant["name"];
+                $tableauImages[$plant["name"]]=$plant["photos"];
+                $tableauDescriptionsBefore[$plant["name"]]=$plant["before"];
+                $tableauDescriptionsAfter[$plant["name"]]=$plant["after"];
+                $tableauXP[$plant["name"]]=$plant["level"];
+            }
+            
+            
             foreach($tableauplant as $plantName) {
+                foreach($tableauImages[$plantName] as $image){
+                    $plantImage=new Image();
+                    $plantImage->setUrl(["/assets/medias/".$image]);
+                    $manager->persist($plantImage);
+                    $manager->flush();
+    
+                };     
                 $plant = new Plant();
                 $plant->setName($plantName);
-                $plant->setLevel();//a changer
-                $plant->setDisplay(1);
+                $plant->setdescription("");
+                $plant->setdescriptionAfter("");
+                $plant->setLevel($tableauXP[$plantName]);
+                $plant->setIsShow(1);
+                $plantImage->setIdPlant($plant);
                 $manager->persist($plant);
                 $manager->flush();
-                $j=0;
-                foreach($tableauImages[$plantName] as $image){
-                $j+=1;
-                $plantImage=new PlantImages();
-                $plantImage->setPlant($plant);
-                $plantImage->setImage("fixtures/".$image);
-                $manager->persist($plantImage);
+            
+            foreach($tableauDescriptionsBefore[$plantName] as $infoElement){
+                $element= new Description();
+                $element->setAfter(0);
+                $element->setPlant($plant);
+                $element->setText($infoElement["text"]);
+                if (isset($infoElement["title"])){
+                    $element->setTitle($infoElement["title"]);
+                }
+                if (isset($infoElement["logo"])){
+                    $element->setLogo($infoElement["logo"]);
+                }
+                $manager->persist($element);
                 $manager->flush();
-            };
+                
+            }
+            foreach($tableauDescriptionsAfter[$plantName] as $infoElement){
+                $element= new Description();
+                $element->setAfter(1);
+                $element->setPlant($plant);
+                $element->setText($infoElement["text"]);
+                if (isset($infoElement["title"])){
+                    $element->setTitle($infoElement["title"]);
+                }
+                if (isset($infoElement["logo"])){
+                    $element->setLogo($infoElement["logo"]);
+                }
+                $manager->persist($element);
+                $manager->flush();
+                
+            }
       }
     }
 }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,15 +11,20 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $options = [
+            'cost' => 4,
+            'time_cost'=> 3, # Lowest possible value for argon
+            'memory_cost'=> 10 # Lowest possible value for argon
+        ];
         $encrypted = password_hash("defaultuser", PASSWORD_BCRYPT, $options);
         $user = new User();
-        $user->setEmail('defaultuser@gmail.com');
+        $user->setEmail('admin@gmail.com');
         $user->setPassword($encrypted);
-        $user->setCreatedAt(new \DateTime());
-        $user->setRoles($roles);
+        $user->setRoles(["ROLE_ADMIN"]);
         $user->setAvatar('assets\medias\user.png');
+        $user->setExp(0);
+        $user->setIsVerified(1);
         $manager->persist($user);
-
-        $manager->flush();
+        $manager->flush();   
     }
 }
